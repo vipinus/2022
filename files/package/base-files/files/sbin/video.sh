24f0c618fa153f356ef4097a98a8210cc909c5ba
vipinus <37501804+vipinus@users.noreply.github.com>
2023-04-14 18:12:22 -0400

    Update video.sh

--- files/package/base-files/files/sbin/video.sh
+++ files/package/base-files/files/sbin/video.sh
@@ -3,4 +3,4 @@
 cp /etc/hosts.temp /etc/hosts                                                                                                       
 nslookup www.netflix.com 127.0.0.1 | grep Address | grep '\.' | grep -v 127.0.0.1 | awk '{print $3,"www.netflix.com"}' >> /etc/hosts                              
 /etc/init.d/dnsmasq restart                                                                                                                                       
-nslookup www.netflix.com 127.0.0.1 | grep Address | grep '\.' | grep -v 127.0.0.1 | awk '{print "iptables -t nat -I zone_lan_prerouting -p tcp -d "$3"/32 -j REDIRECT --to-ports 12345"}' | sh
+nslookup www.netflix.com 127.0.0.1 | grep Address | grep '\.' | grep -v 127.0.0.1 | awk '{print "iptables -t nat -I PREROUTING -p tcp -d "$3"/32 -j REDIRECT --to-ports 12345"}' | sh
