1c2cb9cd294719e254b3d7e6e71ddc33b5fb2f3b
vipinus <37501804+vipinus@users.noreply.github.com>
2023-06-28 16:20:58 -0400

    Update rtab.sh

--- files/package/base-files/files/sbin/rtab.sh
+++ files/package/base-files/files/sbin/rtab.sh
@@ -3,22 +3,11 @@ file="/tmp/rtab.lock"
 if [ ! -f "$file" ]; then
     touch /tmp/rtab.lock
     /etc/init.d/redsocks start
+    ipset restore < /etc/ipsets
     iptables -t nat -N REDSOCKS
-	iptables -t nat	-A REDSOCKS -d 0.0.0.0 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 255.255.255.255 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 10.0.0.0/8 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 127.0.0.0/8 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 169.254.0.0/16 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 172.16.0.0/12 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 192.168.0.0/16 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 224.0.0.0/4 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 240.0.0.0/4 -j RETURN
-	iptables -t nat	-A REDSOCKS -d 255.255.255.255 -j RETURN
-	iptables -t nat	-A REDSOCKS -p tcp -j REDIRECT --to-ports 1081
-	iptables -t nat	-A PREROUTING -p tcp -j REDSOCKS
-    for n in $(cat /etc/chinadns_chnroute.txt)
-    do
-    iptables -t nat -I REDSOCKS -d $n -j RETURN
-    done
+    iptables -t nat -I OUTPUT -p tcp -j REDSOCKS
+    iptables -t nat -I REDSOCKS -m set --match-set redsocks_bypass dst -j RETURN
+    iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 1081
+    iptables -t nat -A PREROUTING -p tcp -j REDSOCKS
     rm /tmp/rtab.lock
 fi
