0cc6b174999af6b15264687a1e728a3689c760e2
vipinus <37501804+vipinus@users.noreply.github.com>
2023-12-09 02:19:03 -0500

    Update rtab.sh

--- files/package/base-files/files/sbin/rtab.sh
+++ files/package/base-files/files/sbin/rtab.sh
@@ -4,11 +4,11 @@ if [ ! -f "$file" ]; then
     touch /tmp/rtab.lock
     /etc/init.d/redsocks start
     ipset restore < /etc/ipsets
-    iptables -t nat -N REDSOCKS
-    iptables -t nat -A OUTPUT -p tcp --dport 22 -j RETURN
-    iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
-    iptables -t nat -A REDSOCKS -m set --match-set redsocks_bypass dst -j RETURN
-    iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 1081
-    iptables -t nat -A PREROUTING -p tcp -j REDSOCKS
+    iptables-nft -t nat -N REDSOCKS
+    iptables-nft -t nat -A OUTPUT -p tcp --dport 22 -j RETURN
+    iptables-nft -t nat -A OUTPUT -p tcp -j REDSOCKS
+    iptables-nft -t nat -A REDSOCKS -m set --match-set redsocks_bypass dst -j RETURN
+    iptables-nft -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 1081
+    iptables-nft -t nat -A PREROUTING -p tcp -j REDSOCKS
     rm /tmp/rtab.lock
 fi
