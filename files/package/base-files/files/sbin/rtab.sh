10300061c441fdd694db986c63c98a16ff560ff1
vipinus <37501804+vipinus@users.noreply.github.com>
2023-12-10 15:14:42 -0500

    Update rtab.sh

--- files/package/base-files/files/sbin/rtab.sh
+++ files/package/base-files/files/sbin/rtab.sh
@@ -10,6 +10,7 @@ if [ ! -f "$file" ]; then
     iptables-nft -t nat -A REDSOCKS -m set --match-set redsocks_bypass dst -j RETURN
     iptables-nft -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 1081
     iptables-nft -t nat -A PREROUTING -p tcp -j REDSOCKS
+    cp -f /etc/config/firewall /etc/config/firewall.reg
     cp -f /etc/config/firewall.vpn /etc/config/firewall
     /etc/init.d/firewall restart
     cp -f /etc/config/firewall.reg /etc/config/firewall
